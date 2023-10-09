1ab16cb7a883cfcc44737cb54f1510337ee09154
tuandao_123 <tuan@nordicalegnagroup.com>
2023-10-09 08:25:19 +0300

    update exercise 10.1-10.2

---
+++ rate-repository-app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports =
+{
+  'plugins': ['react', 'react-native'],
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  },
+  'extends': ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
+  'parser': '@babel/eslint-parser',
+  'env': {
+    'react-native/react-native': true
+  },
+  'rules': {
+    'react/prop-types': 2,
+    'react/react-in-jsx-scope': 0,
+    'jsx-quotes' : ['error', 'prefer-single'],
+    'quotes' : ['error', 'single'],
+    'react-native/no-color-literals': 0,
+  }
+}
+
