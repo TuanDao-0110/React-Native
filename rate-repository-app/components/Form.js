d1b1638651fd187ac8a8a542d22e5f1c1d80bf79
TuanDao-0110 <rickytuan1991@gmail.com>
2023-10-12 16:05:25 +0300

    exercise 10.9

--- rate-repository-app/components/Form.js
+++ rate-repository-app/components/Form.js
@@ -2,11 +2,27 @@
 /* eslint-disable no-dupe-keys */
 /* eslint-disable react-native/no-unused-styles */
 import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, Switch, Text } from 'react-native';
 import { Formik } from 'formik';
 import Button from './Button';
+import { useState } from 'react';
+import * as yup from 'yup';
+const loginValidationSchema = yup.object().shape({
+    email: yup
+        .string()
+        .email('Please enter valid email')
+        .required('Email Address is Required'),
+    password: yup
+        .string()
+        .min(8, ({ min }) => `Password must be at least ${min} characters`)
+        .required('Password is required'),
+})
+
 
 const MyReactNativeForm = props => {
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     const styles = StyleSheet.create({
         loginContainer: {
             width: '100%',
@@ -27,12 +43,19 @@ const MyReactNativeForm = props => {
             borderWidth: StyleSheet.hairlineWidth,
             borderRadius: 10,
         },
+        toggle: {
+            marginTop: 5
+        }, errors:
+            { fontSize: 10, color: 'red' }
+
     })
     return <Formik
         initialValues={{ email: '', password: '' }}
         onSubmit={values => console.log(values)}
+
+        validationSchema={loginValidationSchema}
     >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
+        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
             <View
                 style={styles.loginContainer}
             >
@@ -44,16 +67,30 @@ const MyReactNativeForm = props => {
                     keyboardType='email-address'
                     placeholder='Email Address'
                 />
-
+                {errors.email &&
+                    <Text style={styles.errors}>{errors.email}</Text>
+                }
                 <TextInput
                     onChangeText={handleChange('password')}
                     onBlur={handleBlur('password')}
                     value={values.password}
-                    secureTextEntry
+                    secureTextEntry={!isEnabled}
                     style={styles.textInput}
                     placeholder='Password'
                 />
+                {errors.password &&
+                    <Text style={styles.errors}>{errors.password}</Text>
+                }
                 <Button content={'Submit'} onPress={handleSubmit} />
+                <Switch
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor='#3e3e3e'
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                    style={styles.toggle}
+                />
+                <Text>show the password </Text>
             </View>
         )}
     </Formik>
